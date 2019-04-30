612641d5cc308469266c888bbe8df0f311c27149
Sky <41249671+TheSkyHero@users.noreply.github.com>
2019-04-30 13:26:06 +0530

    Update response.js

--- modules/response.js
+++ modules/response.js
@@ -62,14 +62,14 @@ exports.create = (config) => {
                 },
                 "files": [{
                     "attachment": config.file,
-                    "name": config.filename
+                    "name": config.filename.replace(/ /g, "_")
                 }]
             },
             "text": config.title + " :\r\n" + config.fileurl,
             "file": [config.title, {
                 "files": [{
                     "attachment": config.file,
-                    "name": config.filename
+                    "name": config.filename.replace(/ /g, "_")
                 }]
             }],
             "mention": config.mention,
