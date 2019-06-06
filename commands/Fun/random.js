a1723c79c4281b48101caed93f37f2650432ccad
TheSkyHero <planetearth1402@outlook.com>
2019-06-06 14:46:56 +0530

    Added random.js

---
+++ commands/Fun/random.js
@@ -0,0 +1,24 @@
+module.exports = async message => {
+    try {
+        let numbers = (await mentions.getNumbers(message)).numbers;
+        if (!numbers || numbers.length < 2) {
+            return await response.error({
+                message: message,
+                error: 'You need to specify 2 numbers'
+            });
+        }
+        else {
+            return await response.send(response.create({
+                message: message,
+                author: message.author,
+                title: `Random numbet between ${numbers[0]}, ${numbers[1]}`,
+                fields: {
+                    Result: randomInt(numbers[0], numbers[1])
+                },
+                error: 'Coin fell out of hand'
+            }));
+        }
+    } catch(error) {
+        debug(error);
+    }
+}
