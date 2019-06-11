ea1450fe4d297be324e4c45b42e5f6da4453b208
TheSkyHero <planetearth1402@outlook.com>
2019-06-11 20:47:39 +0530

    Fixed unexpected behavior of mentions.js:getCode()

--- modules/mentions.js
+++ modules/mentions.js
@@ -469,9 +469,10 @@ module.exports.getCompiler = async message => {
 
 module.exports.getCode = async (message, o) => {
     let code = shiftWord(message.content);
-    let flag = o && code.replace(/\r|\n|\t| /g, '').toLowerCase().startsWith(o.toLowerCase());
-    if (flag) {
-        code = code.slice(code.toLowerCase().indexOf(o.toLowerCase()) + o.length);
+    let codeLower = code.toLowerCase();
+    let flag = o;
+    if (flag && codeLower.indexOf(o.toLowerCase()) != -1) {
+        code = code.slice(codeLower.indexOf(o.toLowerCase()) + o.length);
     }
     let clean = () => {
         while ([' ', '\r', '\n', '\t'].includes(code.slice(0, 1))) {
