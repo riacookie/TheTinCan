80fcd85f312360236c6a3966b950ef18f92bc7be
TheSkyHero <planetearth1402@outlook.com>
2019-06-15 21:10:57 +0530

    Small fix.

--- runtime/bot.js
+++ runtime/bot.js
@@ -22,14 +22,14 @@ module.exports = () => new Promise((resolve, reject) => {
     Client.on('message', async message => {
         if (!message.author.bot && message.content.startsWith(process.env.prefix)) {
             let cmd = firstWord(message.content).toLowerCase().replace(process.env.prefix, '');
-            if (bot.commands.cmds[cmd] && (bot.commands.cmds[cmd] == 'identity' || (!(await management.isBlacklisted(mention.user.id))))) {
+            if (bot.commands.cmds[cmd] && (bot.commands.cmds[cmd] == 'identity' || (!(await management.isBlacklisted(message.author.id))))) {
                 try {
                     await require(`../commands/${bot.commands.files[bot.commands.cmds[cmd]]}`)(message);
                 } catch (error) {
                     debug(error);
                 }
             }
-            else if (!(await management.isBlacklisted(mention.user.id)) && wandbox.languages.lower.includes(cmd.toLowerCase()) || wandbox.languages.lower.includes(cmd.toLowerCase() + ' ' + firstWord(shiftWord(message.content.toLowerCase())))) {
+            else if (!(await management.isBlacklisted(message.author.id)) && wandbox.languages.lower.includes(cmd.toLowerCase()) || wandbox.languages.lower.includes(cmd.toLowerCase() + ' ' + firstWord(shiftWord(message.content.toLowerCase())))) {
                 try {
                     await require(`../commands/${bot.commands.files['compile']}`)(message);
                 } catch (error) {
