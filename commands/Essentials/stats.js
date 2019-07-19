7263a02c13cc1c94b37a738f8c92c1a836e8e0cb
TheSkyHero <planetearth1402@outlook.com>
2019-07-19 17:20:29 +0530

    Small change.

--- commands/Essentials/stats.js
+++ commands/Essentials/stats.js
@@ -2,12 +2,11 @@ module.exports = async ({message}) => await response.create({
     message: message,
     title: 'My statistics',
     fields: {
-        'Uptime': (uptime => `Up for ${uptime.days()} days ${uptime.hours()} hours ${uptime.minutes()} minutes ${uptime.seconds()} seconds.`)(moment.duration(client.uptime)),
+        'Uptime': (uptime => `${uptime.days()} days ${uptime.hours()} hours ${uptime.minutes()} minutes ${uptime.seconds()} seconds.`)(moment.duration(client.uptime)),
         'Guilds': client.guilds.size,
         'Connection Speed': `${Math.round(client.ping)}ms`,
-        'Memory Usage': `${((os.totalmem() - os.freemem())/1073741824).toFixed(2)}GB/${(os.totalmem()/1073741824).toFixed(2)}GB`,
-        'Node.js version': process.version,
-        'Discord.js version': discord.version
+        'Discord.js version': discord.version,
+        'Node.js version': process.version
     },
     thumbnail: client.user.displayAvatarURL
 });
