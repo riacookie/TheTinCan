d066769312a1f87243530d328fd4a490fdd79bbf
TheSkyHero <planetearth1402@outlook.com>
2019-07-19 17:17:05 +0530

    Fix.

--- commands/Essentials/stats.js
+++ commands/Essentials/stats.js
@@ -4,8 +4,8 @@ module.exports = async ({message}) => await response.create({
     fields: {
         'Uptime': (uptime => `Up for ${uptime.days()} days ${uptime.hours()} hours ${uptime.minutes()} minutes ${uptime.seconds()} seconds.`)(moment.duration(client.uptime)),
         'Guilds': client.guilds.size,
-        'Connection Speed': Math.round(client.ping),
-        'Memory Usage': `${(os.freemem()/1073741824).toFixed(2)}GB/${(os.totalmem()/1073741824).toFixed(2)}`,
+        'Connection Speed': `${Math.round(client.ping)}ms`,
+        'Memory Usage': `${((os.totalmem() - os.freemem())/1073741824).toFixed(2)}GB/${(os.totalmem()/1073741824).toFixed(2)}GB`,
         'Node.js version': process.version,
         'Discord.js version': discord.version
     },
