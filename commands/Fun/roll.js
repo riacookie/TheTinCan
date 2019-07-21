ed13ca1ce00e27691fb237017d1e627927832ee3
TheSkyHero <planetearth1402@outlook.com>
2019-07-21 18:19:03 +0530

    Added limit in roll command. and small changes.

--- commands/Fun/roll.js
+++ commands/Fun/roll.js
@@ -1,5 +1,9 @@
 module.exports = async ({message, options}) => {
     if (!options.amount) options.amount = 2;
+    if (options.amount > 10) return await response.create({
+        message: message,
+        error: 'You can only roll upto 10 die at once'
+    });
     let results = [];
     for (let i = 1; i <= options.amount; i++) {
         results.push(misc.random.int(1, 6));
