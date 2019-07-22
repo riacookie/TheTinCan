26513c66e63a9bdf4d82039fe346efa0b1a74a19
TheSkyHero <planetearth1402@outlook.com>
2019-07-22 23:56:20 +0530

    Added eval command for debugging purpose.

--- commands/Management/eval.js
+++ commands/Management/eval.js
@@ -1,13 +1,11 @@
 module.exports = async ({message, content}) => {
     const identity = await management.identity.get(message.author.id);
-    if (!identity.permissions.includes('REFRESH')) return await response.custom({
+    if (!identity.permissions.includes('Execute')) return await response.custom({
         message: message,
         error: 'You don\'t have permission to use this command'
     });
-    global['bot_data'] = await firebase.get('/bot');
-    debug(`refreshed database cache.`);
     return await response.custom({
         message: message,
-        fields: 'Successfully refreshed database cache'
+        fields: eval(mentions.getCode(misc.string.shisftWord(content)))
     });
 }
